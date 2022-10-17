0bdb72e7d5f14836bb6a7fa3c600392fe406b6b7
Nikolasramadhan06 <76188437+Nikolasramadhan06@users.noreply.github.com>
2022-10-17 18:18:35 +0700

    Upload Tugas Video 1,2,3

---
+++ coba-laravel/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NKLS | About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3><?= $Nama; ?></h3>
+    <p><?= $email; ?></p>
+    <img src="img/<?= $image; ?>" alt="<?= $Nama; ?>" width="200">
+</body>
+</html>
