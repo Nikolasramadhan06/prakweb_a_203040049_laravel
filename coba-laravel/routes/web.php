b5f10a56c164734318974b9195fb6633982cb850
Nikolasramadhan06 <76188437+Nikolasramadhan06@users.noreply.github.com>
2022-11-05 09:38:06 +0700

    video 11

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -38,24 +38,24 @@ Route::get('/blog', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function(){
-    return view('categoriPes', [
+    return view('categories', [
         'title' => 'Post Categories',
         'categories' => Category::all()
     ]);
 }); 
 
 Route::get('/categories/{category:slug}', function(Category $category){
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts->load('category', 'author')
+        
     ]);
 });
 
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->posts,
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
    
