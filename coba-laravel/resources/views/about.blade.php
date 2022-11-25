d5fa5fa7c2cae87e9495854b2d6078d5fb8e3d42
Nikolasramadhan06 <76188437+Nikolasramadhan06@users.noreply.github.com>
2022-11-25 07:15:09 +0700

    video 16

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section ('container')
     <h1>Halaman About</h1>
-    <h3>{{ $nama }}</h3>
+    <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="img/{{  $image }}" alt="{{ $nama }}" width="200" class="img-thumbnail rounded-circle">
+    <img src="img/{{  $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
 @endsection
---
+++ coba-laravel/resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Nikolas Ramadhan</h1>
