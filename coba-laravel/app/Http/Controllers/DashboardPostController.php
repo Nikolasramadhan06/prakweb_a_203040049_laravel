b13dae7fabf08627390320f3ff966cd9016707e5
Nikolasramadhan06 <76188437+Nikolasramadhan06@users.noreply.github.com>
2022-11-27 19:03:36 +0700

    video 10 video & delete

--- coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -5,108 +5,131 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('dashboard.posts.index', [
-            'posts' => Post::where('user_id', auth()->user()->id)->get()
-        ]);
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return view('dashboard.posts.index', [
+      'posts' => Post::where('user_id', auth()->user()->id)->get()
+    ]);
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('dashboard.posts.create', [
+      'categories' => Category::all()
+    ]);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $validateData = $request->validate([
+      'title' => 'required|max:255',
+      'slug' => 'required|unique:posts',
+      'category_id' => 'required',
+      'body' => 'required'
+    ]);
+
+    $validateData['user_id'] = auth()->user()->id;
+    $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+    Post::create($validateData);
+
+    return redirect('/dashboard/posts')->with('success', 'New post has been added!');
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Post $post)
+  {
+    return view('dashboard.posts.show', [
+      'post' => $post
+    ]);
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Post $post)
+  {
+    return view('dashboard.posts.edit', [
+      'post' => $post,
+      'categories' => Category::all()
+    ]);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Post $post)
+  {
+    $rules = [
+      'title' => 'required|max:255',
+      'category_id' => 'required',
+      'body' => 'required'
+    ];
+
+    if ($request->slug != $post->slug) {
+      $rules['slug'] = 'required|unique:posts';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('dashboard.posts.create', [
-            'categories' => Category::all()
-        ]);
-    }
+    $validatedData = $request->validate($rules);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $validateData = $request->validate([
-            'title' => 'required|max:255',
-            'slug' => 'required|unique:posts',
-            'category_id' => 'required',
-            'body' => 'required'
-          ]);
-      
-          $validateData['user_id'] = auth()->user()->id;
-          $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
-      
-          Post::create($validateData);
-      
-          return redirect('/dashboard/posts')->with('success', 'New post has been added!');
-    }
+    $validatedData['user_id'] = auth()->user()->id;
+    $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        return view('dashboard.posts.show', [
-            'post' => $post
-        ]);
-    }
+    Post::where('id', $post->id)
+      ->update($validatedData);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
+    return redirect('/dashboard/posts')->with('success', 'New post has been update!');
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Post $post)
+  {
+    Post::destroy($post->id);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
+    return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
+  }
 
-    public function checkSlug(Request $request)
-    {
-        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
-        return response()->json(['slug' => $slug]);
-    }
+  public function checkSlug(Request $request)
+  {
+    $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+    return response()->json(['slug' => $slug]);
+  }
 }
