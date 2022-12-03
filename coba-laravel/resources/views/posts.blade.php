d517b687edc39209085003e2743298e19a3c3a61
Nikolasramadhan06 <76188437+Nikolasramadhan06@users.noreply.github.com>
2022-12-03 18:55:38 +0700

    video 23

--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -21,31 +21,33 @@
     </div>
 
     @if ($posts->count())
-    <div class="card mb-3">
-        @if ($posts[0]->image)
-        <div style="max-height: 400px; overflow:hidden">
-            <img src="{{ asset('storage/' . $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}"
-                class="img-fluid">
+        <div class="card mb-3">
+            @if ($posts[0]->image)
+                <div style="max-height: 400px; overflow:hidden">
+                    <img src="{{ asset('storage/' . $posts[0]->image) }}" alt="{{ $posts[0]->category->name }}"
+                        class="img-fluid">
+                </div>
+            @else
+                <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top"
+                    alt="{{ $posts[0]->category->name }}">
+            @endif
+            <div class="card-body text-center">
+                <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
+                <p>
+                    <small class="text-muted">By.<a
+                            href="/blog?author={{ $posts[0]->author->username }}"class="text-decoration-none">{{ $posts[0]->author->name }}</a>
+                        in
+                        <a
+                            href="/blog?category={{ $posts[0]->category->slug }}"class="text-decoration-none">{{ $posts[0]->category->name }}</a>
+                        {{ $posts[0]->created_at->diffForHumans() }}
+                    </small>
+                </p>
+
+                <p class="card-text">{{ $posts[0]->excerpt }}</p>
+                <a href="/posts/{{ $posts[0]->slug }}"class="text-decoration-none btn btn-primary">Read more</a>
+            </div>
         </div>
-    @else
-        <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top"
-            alt="{{ $posts[0]->category->name }}">
-    @endif
-    alt="{{ $posts[0]->category->name }}">
-        <div class="card-body text-center">
-            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
-            <p>
-                <small class="text-muted">
-                    By. <a href="/blog?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/blog?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }} 
-                </small>
-            </p>
-
-            <p class="card-text">{{ $posts[0]->excerpt }}</p>
-
-            <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more</a>
-
-        </div>
-    </div>
     
 
     <div class="container">
